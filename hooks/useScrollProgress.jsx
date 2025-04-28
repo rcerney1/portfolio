a1378b9d0affb67bf1deec53445364dc2714781f
rcerney <ryancerney1@gmail.com>
2025-04-28 13:37:01 -0700

    header

---
+++ hooks/useScrollProgress.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const useScrollProgress = () => {
+    const [completion, setCompletion] = useState(0);
+
+    useEffect(() => {
+        const updateScrollCompletion = () => {
+            const currentProgress = window.scrollY;
+            const scrollHeight = document.body.scrollHeight - window.innerHeight;
+
+            if (scrollHeight) {
+                setCompletion(Number((currentProgress / scrollHeight) * 100).toFixed(2));
+            }
+        };
+
+        window.addEventListener('scroll', updateScrollCompletion);
+
+        return () => window.removeEventListener('scroll', updateScrollCompletion);
+    }, []);
+
+    return completion;
+};
+
+export default useScrollProgress;
