c13cd1cb7f77a7bb658fdf305171a285e14cc5c0
rcerney <ryancerney1@gmail.com>
2025-06-10 20:45:23 -0700

    more testing

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,12 @@ import Cta from '@/components/Cta';
 export default function Home() {
   return (
     <main>
+      <form name="contact" data-netlify="true" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message" />
+      </form>
+
       <Hero />
       <About />
       <Services />
