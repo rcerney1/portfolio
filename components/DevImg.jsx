464eb2890759cb3fbe9ae5734ea720aeda40066f
rcerney <ryancerney1@gmail.com>
2025-06-10 21:05:32 -0700

    test

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const DevImg = ({ containerStyles, imgSrc }) => {
   return (
-    <div className={`${containerStyles}`}>
-      <Image src={imgSrc} fill priority alt='' />
+    <div className={`${containerStyles} relative w-full h-full`}>
+      <Image src={imgSrc} fill priority alt='Developer Image' />
     </div>
   );
 };
