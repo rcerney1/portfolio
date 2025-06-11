b2b3473e0a9f0cb397e27928af72a901a1eca726
rcerney <ryancerney1@gmail.com>
2025-06-10 20:28:42 -0700

    test form

--- components/Form.jsx
+++ components/Form.jsx
@@ -7,27 +7,41 @@ import { User, MailIcon, ArrowRightIcon, MessageSquare } from 'lucide-react';
 
 const Form = () => {
   return (
-    <form name="contact" netlify className='flex flex-col gap-y-4'>
-      {/* input */}
-      <div className='relative flex items-center'>
-        <Input type='name' id='name' placeholder='Name' />
-        <User className='absolute right-6' size={20} />
-      </div>
-      {/* input */}
-      <div className='relative flex items-center'>
-        <Input type='email' id='email' placeholder='Email' />
-        <MailIcon className='absolute right-6' size={20} />
-      </div>
-      {/* textarea */}
-      <div className='relative flex items-center'>
-        <Textarea placeholder='Type Your Message Here.' />
-        <MessageSquare className='absolute top-4 right-6' size={20} />
-      </div>
-      <Button className='flex items-center gap-x-1 max-w-[166px]'>
-        Let's Talk
-        <ArrowRightIcon size={20} />
-      </Button>
-    </form>
+    <>
+      <form name="contact" netlify className='flex flex-col gap-y-4'>
+        {/* input */}
+        <div className='relative flex items-center'>
+          <Input type='name' id='name' placeholder='Name' />
+          <User className='absolute right-6' size={20} />
+        </div>
+        {/* input */}
+        <div className='relative flex items-center'>
+          <Input type='email' id='email' placeholder='Email' />
+          <MailIcon className='absolute right-6' size={20} />
+        </div>
+        {/* textarea */}
+        <div className='relative flex items-center'>
+          <Textarea placeholder='Type Your Message Here.' />
+          <MessageSquare className='absolute top-4 right-6' size={20} />
+        </div>
+        <Button className='flex items-center gap-x-1 max-w-[166px]'>
+          Let's Talk
+          <ArrowRightIcon size={20} />
+        </Button>
+      </form>
+      <form name="contact" netlify>
+        <p>
+          <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </>
+
   );
 };
 
