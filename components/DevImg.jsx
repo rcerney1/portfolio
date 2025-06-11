4a5b30183ed213a47e711713ef0d35565eebea79
rcerney <ryancerney1@gmail.com>
2025-06-10 21:17:16 -0700

    placeholder dev img

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -3,9 +3,7 @@ import Image from 'next/image';
 const DevImg = ({ containerStyles, imgSrc }) => {
   return (
     <div className={`${containerStyles}`}>
-      {/* <Image src={imgSrc} fill priority alt='' /> */}
-      <img src={imgSrc} alt='dev' className='w-[300px] h-auto border border-red-500' />
-
+      <Image src={imgSrc} fill priority alt='' />
     </div>
   );
 };
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -73,6 +73,11 @@ const Hero = () => {
               containerStyles='bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom'
               imgSrc='/hero/developer5.png'
             />
+            <DevImg
+              containerStyles='relative w-[300px] h-[300px]'
+              imgSrc='https://via.placeholder.com/300'
+            />
+
           </div>
         </div>
         {/* icon */}
@@ -80,7 +85,7 @@ const Hero = () => {
           <RiArrowDownSLine className='text-3xl text-primary' />
         </div>
       </div>
-      
+
     </section>
   );
 };
